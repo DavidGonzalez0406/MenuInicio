1b46346ff311d2299492e8cbae860cdac55017d7
DavidGonzalez0406 <132014778+DavidGonzalez0406@users.noreply.github.com>
2024-03-03 01:32:21 -0600

    Add files via upload

---
+++ MenuInicio/src/menuinicio/MenuInicio.java
@@ -0,0 +1,17 @@
+
+package menuinicio;
+
+/**
+ *
+ * @author glori
+ */
+public class MenuInicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
